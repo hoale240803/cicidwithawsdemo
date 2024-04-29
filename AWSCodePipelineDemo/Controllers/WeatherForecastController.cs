2d66b02b34e28383e727017bfdd03a672227aba8
Hoa Le <56470482+hoale240803@users.noreply.github.com>
2024-04-29 22:14:57 +0700

    updateSummaries  Balmy444444444

--- AWSCodePipelineDemo/Controllers/WeatherForecastController.cs
+++ AWSCodePipelineDemo/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace AWSCodePipelineDemo.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Scorching2", "Scorching3"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy444444444", "Hot", "Sweltering", "Scorching", "Scorching2", "Scorching3"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
